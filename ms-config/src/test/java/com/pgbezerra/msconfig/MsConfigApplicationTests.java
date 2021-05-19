91404434640ce84b99f0b1cc1a5f902bbb80091e
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2021-05-19 16:20:13 -0300

    Config server and external properties

---
+++ ms-config/src/test/java/com/pgbezerra/msconfig/MsConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgbezerra.msconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
