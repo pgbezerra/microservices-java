5e5ca8425fe9acd31f42e205904914746b4d8bd1
Paulo Gabriel Justino Bezerra <pgj1085@gmail.com>
2021-05-23 20:07:52 -0300

    prometheus, grafana, zipkin, logspout, logstash, kibana, elasticsearch, update product insert log and docker-compose update

--- product-service/src/main/java/com/pgbezerra/productservice/listener/ProductPersistLogListener.java
+++ product-service/src/main/java/com/pgbezerra/productservice/listener/ProductPersistLogListener.java
@@ -1,6 +1,7 @@
 package com.pgbezerra.productservice.listener;
 
 import com.pgbezerra.productservice.event.ProductPersistEvent;
+import com.pgbezerra.productservice.model.Product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
@@ -9,10 +10,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class ProductPersistLogListener  implements ApplicationListener<ProductPersistEvent> {
 
-    private static final Logger log = LoggerFactory.getLogger(ProductPersisQueueListener.class);
+    private static final Logger log = LoggerFactory.getLogger(ProductPersistLogListener.class);
 
     @Override
     public void onApplicationEvent(ProductPersistEvent productPersistEvent) {
-        log.info(productPersistEvent.getProduct().toString());
+        final Product product = productPersistEvent.getProduct();
+        log.info(String.format("New product inserted: %s", product.toString()));
     }
 }
